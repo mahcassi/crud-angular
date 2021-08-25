baf90d300b2e238f0e90c9b75d469a463d2fd35d
Maria Eduarda <m.cassiano@avivatec.com.br>
2021-08-25 13:46:48 -0300

    Product service

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
-  }, {
+  }, 
+  {
     path: "products",
     component: ProductCrudComponent
+  }, 
+  {
+    path: "products/create",
+    component: ProductCreateComponent
   }
 ];
 
